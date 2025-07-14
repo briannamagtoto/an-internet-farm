a03e067bd6e728f4304e809ab5a27223b434e0cf
briannamagtoto <briannamagtoto@gmail.com>
2025-07-13 23:14:02 -0700

    Remove scrolling behavior

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// Desktop/laptop scrolling
-window.addEventListener('wheel', function(event) {
-    event.preventDefault();
-    
-    // Detect if it's a trackpad (smaller deltaY values) or mouse wheel
-    let scrollAmount = Math.abs(event.deltaY) < 50 ? event.deltaY * 8 : event.deltaY * 0.6;
-    
-    window.scrollBy({
-        top: scrollAmount,
-        behavior: 'smooth'
-    });
-}, { passive: false });
-
 // loading screen for internal navigation
 document.addEventListener('DOMContentLoaded', function() {
     const internalLinks = document.querySelectorAll('a[href^="articles"], a[href^="a-zine"], a[href^="notes"]');
